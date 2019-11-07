0d5b98cbe4c28a396ff124d0d6ed67dfc8619d57
HafeezBrothers <56994356+HafeezBrothers@users.noreply.github.com>
2019-11-07 15:49:37 +0500

    Update __manifest__.py

--- so_workflows_level_3/__manifest__.py
+++ so_workflows_level_3/__manifest__.py
@@ -4,6 +4,7 @@
     'description' : 'SO WorkFlows',
     'author' : 'Hafeez Brothers',
     'version': '1.0',
+    'license': 'LGPL-3',
     'depends' : [
                    'base', 'product','sale',
                 ],
